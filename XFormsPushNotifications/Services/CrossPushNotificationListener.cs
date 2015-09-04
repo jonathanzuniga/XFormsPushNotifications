c4f31685190019193bf75694ec56a9e872312213
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2015-09-04 14:17:05 -0700

    Initial commit.

---
+++ XFormsPushNotifications/Services/CrossPushNotificationListener.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using PushNotification.Plugin;
+using System.Collections.Generic;
+using PushNotification.Plugin.Abstractions;
+using System.Diagnostics;
+
+namespace XFormsPushNotifications
+{
+	public class CrossPushNotificationListener : IPushNotificationListener
+	{
+		public void OnMessage (IDictionary<string, object> Parameters, DeviceType deviceType)
+		{
+
+		}
+
+		public void OnRegistered (string Token, DeviceType deviceType)
+		{
+
+		}
+
+		public void OnUnregistered (DeviceType deviceType)
+		{
+
+		}
+
+		public void OnError (string message, DeviceType deviceType)
+		{
+
+		}
+
+		// Fires when error.
+		void IPushNotificationListener.OnError(string message, DeviceType deviceType)
+		{
+			Debug.WriteLine(string.Format("Push notification error - {0}", message));
+		}
+
+		// Enable/Disable showing the notification.
+		bool IPushNotificationListener.ShouldShowNotification()
+		{
+			return true;
+		}
+	}
+}
---
+++ XFormsPushNotifications/Services/IAzureNotificationHubService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace XFormsPushNotifications
+{
+	public interface IAzureNotificationHubService
+	{
+		Task RegisterNativeAsync(string deviceId);
+		Task UnregisterNativeAsync ();
+	}
+}
